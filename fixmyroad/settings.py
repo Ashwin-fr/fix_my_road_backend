dc3357094ca952144cca11e6c08889b167e6b583
Ashwin <Ashwinchandran001@gmail.com>
2025-11-27 19:23:01 +0530

    django cores

--- fixmyroad/settings.py
+++ fixmyroad/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -142,3 +144,5 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "api.backends.EmailBackend"
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
